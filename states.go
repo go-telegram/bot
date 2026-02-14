76e02846c49fd132c15b5f7a086ba86222f879b1
amaterasutears <kirillufo@gmail.com>
2026-02-14 21:06:31 +0300

    feat: add states

---
+++ states.go
@@ -0,0 +1,12 @@
+package bot
+
+import "github.com/go-telegram/bot/internal/machine"
+
+type states struct {
+	def machine.State
+	cur machine.State
+}
+
+func (s states) isEqual() bool {
+	return s.def == s.cur
+}
